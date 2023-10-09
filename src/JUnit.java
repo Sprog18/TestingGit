64e2bba15574f97d019400abdd921f1cd89b0e3c
ATCHLEYJH22 <atchleyJH22@gcc.edu>
2023-10-09 13:34:31 -0400

    Initial commit

---
+++ src/JUnit.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class JUnit {
+    public static int findMax(ArrayList<Integer> xs) {
+        int max = 0;
+        for (int x : xs) {
+            if (x > max) {
+                max = x;
+            }
+        }
+        return max;
+    }
+}
